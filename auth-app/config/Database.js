a0fbe87cffbd45eeede2149189794fa2f541dcab
Ridho RP <ridhorachmatp@gmail.com>
2023-07-28 13:25:53 +0700

    config sequelize

---
+++ auth-app/config/Database.js
@@ -0,0 +1,8 @@
+const Sequelize = require("sequelize");
+
+const db = new Sequelize(process.env.DB_NAME, process.env.DB_USERNAME, "", {
+  host: process.env.DB_HOST,
+  dialect: DB_DIALECT,
+});
+
+module.exports = db;
